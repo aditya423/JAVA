23e1c2a1d988dd4dfbf7b274b6137cd5b22c51cf
aditya423 <adityasghadge@gmail.com>
2022-04-28 18:10:16 +0530

    first

---
+++ PRACTICALS/Helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld {
+
+    public static void main(String args[]){
+
+        System.out.println("Hello World!");
+
+	// System – is a final class in java.lang package
+	// out – is a static member field of System class 
+	// println – is a method of PrintStream class to print the statement
+    }
+}
