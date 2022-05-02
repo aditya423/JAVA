cd60cd82396b30dcc31d9f981ea230558b6c60b8
aditya423 <adityasghadge@gmail.com>
2022-05-02 18:53:17 +0530

    02-05-2022 [18:53]

---
+++ PRACTICALS/CharDataType.java
@@ -0,0 +1,13 @@
+package data_types;
+
+public class CharDataType {                        // size = 2 bytes (16 bits)
+
+	public static void main(String[] args) {
+
+		char marks,grade;
+		marks='8';
+		grade='B';
+		System.out.println("Marks: " + marks);
+		System.out.println("Grade: " + grade);
+	}
+}
