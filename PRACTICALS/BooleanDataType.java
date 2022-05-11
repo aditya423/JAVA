978ab51217dfe33e4fd809c42ab0cb27b42ac992
aditya423 <adityasghadge@gmail.com>
2022-05-11 18:54:02 +0530

    05-05-2022 [18:00]

---
+++ PRACTICALS/BooleanDataType.java
@@ -0,0 +1,11 @@
+package data_types;
+
+public class BooleanDataType {
+
+	public static void main(String[] args) {
+		
+		boolean myBool = true;
+		if(myBool == true)
+		System.out.println("I am using a Boolean data type = " + myBool);
+	}
+}
