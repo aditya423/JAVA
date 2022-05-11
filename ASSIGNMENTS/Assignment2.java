4f460ecc0d07496e0d6bc1c7d3cd696abe10e38b
aditya423 <adityasghadge@gmail.com>
2022-05-11 19:01:38 +0530

    05-05-2022 [18:00]

---
+++ ASSIGNMENTS/Assignment2.java
@@ -0,0 +1,27 @@
+package assignments;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		
+		char ch = 'A';
+		byte bt = 100;
+		short sh = 1234;
+		int in = 12453;
+		long ln = 2142353l;
+		float fl = 143.232f;
+		double db = 2152.6354d;
+		boolean B = true;
+		
+		if(B == true) {
+			System.out.println("Character is " + ch);
+			System.out.println("Byte is " + bt);
+			System.out.println("Short is " + sh);
+			System.out.println("Int is " + in);
+			System.out.println("Long is " + ln);
+			System.out.println("Float is " + fl);
+			System.out.println("Double is " + db);
+			System.out.println("Boolean is " + B);
+		}
+	}
+}
